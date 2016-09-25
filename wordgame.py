7b78fdeefef37b775955918887abac9c6a0335bd
Hilary Martens <hmartens@Hilarys-MacBook-Pro.local>
2016-09-25 16:15:20 -0600

    Finished code.

--- wordgame.py
+++ wordgame.py
@@ -14,6 +14,20 @@ def get_guess():
     print("Enter a letter")
     return input()
 
+def process_letter(letter,secret_word,blanked_word):
+    result = False
+    for ii in range(0,len(secret_word)):
+        if secret_word[ii] == letter:
+            result = True
+            blanked_word[ii] = letter
+    return result
+
+def print_strikes(strikes):
+    print('Strike', strikes, '!')
+    for ii in range(0,strikes):
+        print("X ", end="")
+    print("")
+
 def play_word_games(): # parameters go inside parentheses
     strikes = 0
     max_strikes = 3
@@ -21,17 +35,24 @@ def play_word_games(): # parameters go inside parentheses
 
     word = get_random_word()
 
-    blanked_word = "_" * len(word)
+    blanked_word = list("_" * len(word))
 
     while playing:
         show_word(blanked_word)
 
         letter = get_guess()
-
-        strikes += 1
+        found = process_letter(letter,word,blanked_word)
+        
+        if not found:
+            strikes += 1
+            print_strikes(strikes)
 
         if strikes >= max_strikes:
             playing = False
+        
+        if not "_" in blanked_word:
+            playing = False
+            print(' '.join(blanked_word))
 
     if strikes >= max_strikes:
         print ("Sorry. Game over.")
